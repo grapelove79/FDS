9c0582393cf04459db92ed844b354cc5358542aa
grapelove <grapelove79@gmail.com>
2016-08-04 09:20:19 +0900

    update Day27

---
+++ Day27/js/01-summary.js
@@ -0,0 +1,15 @@
+// BOM(Browser Object Model)
+// Object
+// Global Object - window
+// window.location
+// window.history
+// window.screen
+// window.navigator
+
+// DOM(Document Object Model) Interface
+// -------------------------------------
+// DOM Legacy (Lv.0)
+// DOM Lv 1
+// DOM Lv 2
+// DOM Lv 3
+// DOM Lv 4
